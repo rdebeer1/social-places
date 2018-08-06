7f34fd3b46a912c96d5c724d249ee2f2a9957beb
rdebeer1 <robert.debeer@gmail.com>
2018-08-06 15:29:11 -0500

    adds deleteHandler to touch event for place output

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     places: []
   };
 
-  placeAddedHandler = (placeName) => {
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
         places: prevState.places.concat(placeName)
@@ -17,11 +17,21 @@ export default class App extends Component {
     });
   };
 
+  placeDeletedHandler = index => {
+    this.setState(prevState => {
+      return {
+        places: prevState.places.filter((place, i) => {
+          return i !== index;
+        })
+      };
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} />
+        <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler} />
       </View>
     );
   }
