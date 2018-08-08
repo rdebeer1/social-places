316aa17425af77ef8132cc87c6adffe9887d90fe
rdebeer1 <robert.debeer@gmail.com>
2018-08-07 19:21:05 -0500

    implements navigation

--- App.js
+++ App.js
@@ -1,70 +1,14 @@
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { connect } from 'react-redux';
+import { Navigation } from 'react-native-navigation';
+import AuthScreen from './src/screens/Auth/Auth'
 
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceList from './src/components/PlaceList/PlaceList';
-import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
-import * as actions from './src/store/actions/index';
-
-class App extends Component {
-
-  placeAddedHandler = placeName => {
-    this.props.onAddPlace(placeName);
-  };
-  
-  placeDeletedHandler = () => {
-    this.props.onDeletePlace();
-  }
-  
-  placeSelectedHandler = key => {
-    this.props.onSelectPlace(key);
-  }
-  
-  modalClosedHandler = () => {
-    this.props.onDeselectPlace();
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetail 
-          selectedPlace={this.props.selectedPlace}
-          onItemDeleted={this.placeDeletedHandler}
-          onModalClose={this.modalClosedHandler} />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList 
-          places={this.props.places} 
-          onItemSelected={this.placeSelectedHandler} />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 45,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  }
+//Register Screens 
+Navigation.registerComponent('social-places.AuthScreen', () => AuthScreen);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'social-places.AuthScreen'
+      }
+    }
+  });
 });
-
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: (name) => dispatch(actions.addPlace(name)),
-    onDeletePlace: () => dispatch(actions.deletePlace()),
-    onSelectPlace: (key) => dispatch(actions.selectPlace(key)),
-    onDeselectPlace: () => dispatch(actions.deselectPlace())
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/screens/Auth/Auth.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AuthScreen extends Component {
+  render() {
+    return(
+      <View style={{padding: 50}}>
+        <Text>Auth Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default AuthScreen;
