769af925590fe6a73c5926cc1a34661db4e79916
rdebeer1 <robert.debeer@gmail.com>
2018-08-13 12:32:02 -0500

    adds platform specific styling for BackgroundButton component

--- src/components/UI/BackgroundButton/BackgroundButton.js
+++ src/components/UI/BackgroundButton/BackgroundButton.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+import { TouchableOpacity, TouchableNativeFeedback, Text, View, StyleSheet, Platform } from 'react-native';
 
-const backgroundButton = props => (
-  <TouchableOpacity 
-    onPress={props.onPress}
-    style={styles.touchableOpacity}>
+const backgroundButton = props => {
+
+  const content = (
     <View 
       style={[
         styles.backgroundButton, 
         {backgroundColor: props.color, borderColor: props.color}]}>
       <Text style={styles.text}>{props.children}</Text>
     </View>
-  </TouchableOpacity>
-);
+  )
+
+  if (Platform.OS === 'android') {
+    return (
+      <View style={styles.touchableOpacity}>
+        <TouchableNativeFeedback 
+          onPress={props.onPress}>
+          {content}
+        </TouchableNativeFeedback>
+      </View>
+    );
+  }
+
+  return (
+    <TouchableOpacity 
+      onPress={props.onPress}
+      style={styles.touchableOpacity}>
+      {content}
+    </TouchableOpacity>
+  );
+}
 
 const styles = StyleSheet.create({
   touchableOpacity: {
