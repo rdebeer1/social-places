aa8bcc50627b50072b9c30a261a4d19fdf2a5428
rdebeer1 <robert.debeer@gmail.com>
2018-08-13 15:25:33 -0500

    adds animation to places list

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -12,7 +12,8 @@ class FindPlaceScreen extends Component {
 
     state = {
         placesLoaded: false,
-        removeAnim: new Animated.Value(1)
+        removeAnim: new Animated.Value(1),
+        placesAnim: new Animated.Value(0)
     }
 
      constructor(props) {
@@ -43,12 +44,25 @@ class FindPlaceScreen extends Component {
         });
     }
 
+    placesLoadedHandler = () => {
+        Animated.timing(this.state.placesAnim, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true
+        }).start();
+    }
+
     placesSearchHandler = () => {
         Animated.timing(this.state.removeAnim, {
             toValue: 0,
             duration: 500,
             useNativeDriver: true
-        }).start();
+        }).start(() => {
+            this.setState({
+                placesLoaded: true
+            });
+            this.placesLoadedHandler();
+        });
     }
 
     render () {
@@ -80,9 +94,14 @@ class FindPlaceScreen extends Component {
 
         if (this.state.placesLoaded) {
             content = (
-                <PlaceList 
-                    places={this.props.places} 
-                    onItemSelected={this.itemSelectedHandler} />
+                <Animated.View
+                    style={{
+                        opacity: this.state.placesAnim
+                    }}>
+                    <PlaceList 
+                        places={this.props.places} 
+                        onItemSelected={this.itemSelectedHandler} />
+                </Animated.View>
             );
         }
 
